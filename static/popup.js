f10f7dba2fae7c4a42818eaeeddde4bd478f170f
Thales Dias da Silva <thales.silva@beteltecnologia.net.br>
2026-05-28 10:44:40 -0300

    corrige endpoint railway

--- static/popup.js
+++ static/popup.js
@@ -227,7 +227,7 @@ startBtn.onclick = async () => {
                 const response =
                     await fetch(
 
-                        'http://127.0.0.1:8080/transcrever',
+                        '/transcrever',
 
                         {
                             method: 'POST',
